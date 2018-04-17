79615b20b3d9ce48f80016c4dd88dc62c1519d1c
Davi Miranda Goncalves <dmgoncalves@latam.stefanini.com>
2018-04-17 18:06:50 -0300

    Aula 5(O commit antigo foi da aula 4)

--- java/parte2/bytebank/src/br/com/alura/Conta.java
+++ java/parte2/bytebank/src/br/com/alura/Conta.java
@@ -2,10 +2,10 @@ package br.com.alura;
 
 public class Conta {
 
-	double saldo;
-	int agencia;
-	int numero;
-	Cliente titular;
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
 
 	public void deposita(double valor) {
 		this.saldo = this.saldo + valor;
@@ -28,4 +28,33 @@ public class Conta {
 		}
 		return false;
 	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
 }
---
+++ java/parte2/bytebank/src/br/com/alura/TesteSacaNeagivo.java
@@ -0,0 +1,14 @@
+package br.com.alura;
+
+public class TesteSacaNeagivo {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);
+		System.out.println(conta.saca(101));
+
+		conta.saca(101);
+
+		System.out.println(conta.getSaldo());
+
+	}
+}
