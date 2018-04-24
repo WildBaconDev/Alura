82aaedabaad6738e29df0474b5ce3abfc04db92c
Davi Miranda Goncalves <dmgoncalves@latam.stefanini.com>
2018-04-24 18:23:10 -0300

    Primeiro commit do curso java parte 3

---
+++ java/Curso_Java_parte_3_Entendendo_herança_e_interface/bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,12 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario funcionario = new Funcionario();
+		
+		funcionario.setNome("teste");
+		funcionario.setCpf("12345678910");
+		funcionario.setSalario(2500.00);
+	}
+}
